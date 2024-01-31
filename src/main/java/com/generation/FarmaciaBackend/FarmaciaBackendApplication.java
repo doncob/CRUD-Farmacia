57e28f656705cc8a9548a76eb0380d817a4ba8c3
rafa v <r_venancio@protonmail.com>
2024-01-31 12:31:07 -0300

    Configurando o projeto

---
+++ src/main/java/com/generation/FarmaciaBackend/FarmaciaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.generation.FarmaciaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/FarmaciaBackend/FarmaciaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.FarmaciaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
